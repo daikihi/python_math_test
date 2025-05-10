9c222037bcb79f9c55b0cc5862410dd253ded788
daiki <daikihi+github@gmail.com>
2025-05-10 13:38:48 +0900

    add variance

---
+++ src/variance.py
@@ -0,0 +1,13 @@
+
+def variance(data_array):
+    """ """
+    if len(data_array) == 0:
+        return 0
+
+    print("=========", data_array)
+    mean = sum(data_array) / len(data_array)
+
+    print("mean", mean)
+    p2s = map(lambda x: (x - mean) ** 2, data_array)
+    variance = sum(p2s) / len(data_array)
+    return variance
---
+++ test/test_variance.py
@@ -0,0 +1,13 @@
+import unittest
+from src.variance import variance
+
+class TestVariance(unittest.TestCase):
+    def test_variance(self):
+        assert variance([1, 2, 3, 4, 5]) == 2.0
+        assert variance([1, 2, 3]) == 2/3
+        assert variance([1]) == 0.0
+        assert variance([]) == 0.0
+        assert variance([1, 2, 3, 4]) == 1.25
+        assert variance([10, 20, 30]) == 200.0/3
+        assert variance([-1, -2, -3]) == 2/3
+        assert variance([1.5, 2.5, 3.5]) == 2/3
