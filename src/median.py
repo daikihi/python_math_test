98f9e45c7247cce6a8fa3939cc3624cf2a50a277
daiki <daikihi+github@gmail.com>
2025-05-14 08:14:26 +0900

    median is defined Median class

--- src/median.py
+++ src/median.py
@@ -1,32 +1,37 @@
 import math
+import statistics
 
 from src.mean import Mean
 
+class Median:
+    def median(self,data_array):
+        """
+        return the median of a list of numbers
+        if the list is empty, return 0
+        if the list has an even number of elements, return the average of the two middle elements
+        if the list has an odd number of elements, return the middle element
+        """
+        if len(data_array) == 0:
+            return [0]
+        elif len(data_array) == 1:
+            return data_array[0]
+        else:
+            data_array.sort()
+            indexes  = self._get_median_index(data_array)
+            response_values = []
+            for i in indexes: 
+                response_values.append(data_array[i - 1]) # array index starts from 0
+            return Mean().mean(response_values)
 
-def median(data_array):
-    """
-    return the median of a list of numbers
-    if the list is empty, return 0
-    if the list has an even number of elements, return the average of the two middle elements
-    if the list has an odd number of elements, return the middle element
-    """
-    if len(data_array) == 0:
-        return [0]
-    elif len(data_array) == 1:
-        return data_array[0]
-    else:
-        data_array.sort()
-        indexes  = _get_median_index(data_array)
-        response_values = []
-        for i in indexes: 
-            response_values.append(data_array[i - 1]) # array index starts from 0
-        return Mean().mean(response_values)
+    def lib_used_median(self,data_array):
+        return statistics.median(data_array)
 
-
-def _get_median_index(data_array):
-    """ """
-    if len(data_array) % 2 == 1:
-        target_indexes = [int((len(data_array) + 1 ) / 2)]
-    else:
-        target_indexes = [int(math.ceil(len(data_array) / 2)), int(math.ceil((len(data_array) + 1)  / 2))]
-    return target_indexes 
+    # should be private and used only for median function
+    def _get_median_index(self, data_array):
+        """ """
+        if len(data_array) % 2 == 1:
+            target_indexes = [int((len(data_array) + 1 ) / 2)]
+        else:
+            target_indexes = [int(math.ceil(len(data_array) / 2)), int(math.ceil((len(data_array) + 1)  / 2))]
+        return target_indexes 
+    
--- test/test_median.py
+++ test/test_median.py
@@ -1,24 +1,24 @@
 import unittest
-from src.median import median 
+from src.median import Median 
 
 class TestMedian(unittest.TestCase):
     def test_median(self):
         """ empty array returns 0"""
-        self.assertEqual(median([]), [0])
+        self.assertEqual(Median().median([]), [0])
         """ single element of array returns the element value"""
-        single_result = median([5])
+        single_result = Median().median([5])
         self.assertEqual(single_result, 5)
         """ multiple elements of array returns the median value"""
-        multiple_result = median([1, 2, 3, 4, 5])
+        multiple_result = Median().median([1, 2, 3, 4, 5])
         self.assertEqual(multiple_result, 3)
         """ even number of elements of array returns the average of the two middle elements"""
-        even_result = median([1, 2, 3, 4])
+        even_result = Median().median([1, 2, 3, 4])
         self.assertEqual(even_result, 2.5)
         """ negative elements of array returns the median value"""
-        negative_result = median([-1, -2, -3, -4, -5])
+        negative_result = Median().median([-1, -2, -3, -4, -5])
         self.assertEqual(negative_result, -3)
         """ mixed elements of array returns the median value"""
-        mixed_result = median([-1, 2, -3, 4, -5])
+        mixed_result = Median().median([-1, 2, -3, 4, -5])
         self.assertEqual(mixed_result, -1)
 
 if __name__ == "__main__":
